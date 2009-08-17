2c419b96efcc9d0046d505ea32591161c0877d7f
Acyuta <acyuta108@gmail.com>
2009-08-17 08:46:11 +0200

    Fixed requirement for trans and purport for multi-verses e.g.: CC Madhya 5.77-78. Production updated included in commit

--- apps/compile/production.js
+++ apps/compile/production.js
@@ -13184,7 +13184,7 @@ console.error("ref argument missing in Quote.need_section");
 }
 return;
 }
-if(/^(SB \d+.\d+.\d+|BG \d+.\d+|CC (Adi|Madhya|Antya) \d+.\d+|NOI \d+|ISO \d+|MM \d+|NBS \d+|LOB \d+)$/.test(_9)){
+if(/^(SB \d+.\d+.\d+(?:-\d+)|BG \d+.\d+(?:-\d+)|CC (Adi|Madhya|Antya) \d+.\d+(?:-\d+)|NOI \d+|ISO \d+|MM \d+|NBS \d+|LOB \d+)$/.test(_9)){
 if(window.console){
 console.info("Quote#need_section: quote "+_9+" needs trans/purport section");
 }
