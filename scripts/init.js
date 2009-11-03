63b2e7561df9d966669c1e11c8f0596deb4176f9
Acyuta <acyuta108@gmail.com>
2009-11-03 14:41:50 -0500

    Switch init to  production

--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,1 @@
-document.write("<script type='text/javascript' src='/js/compile-2.0/jmvc/include.js?compile,development'></script>");
+document.write("<script type='text/javascript' src='/js/compile-2.0/jmvc/include.js?compile,production'></script>");
